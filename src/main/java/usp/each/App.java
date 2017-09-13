46b821ee4b59bcf36fe4c6e841298daa501c805e
lbertoni <lbertoni@uolinc.com>
2017-09-13 20:40:13 -0300

    Novos métodos com as querys necessárias

--- src/main/java/usp/each/App.java
+++ src/main/java/usp/each/App.java
@@ -5,57 +5,45 @@ import java.sql.ResultSet;
 import usp.each.task.ConnectionFactory;
 import usp.each.task.OrderTask;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         new App().startApp();
     }
 
-    public void startApp()
-    {
+    public void startApp() {
         Runtime.getRuntime().addShutdownHook(new Thread(() -> closeApp()));
         runTestQueries();
     }
 
-    public void closeApp()
-    {
+    public void closeApp() {
         ConnectionFactory.closeConnection();
         System.out.println("Tchau!");
     }
 
-    public void runTestQueries()
-    {
-        try
-        {
+    public void runTestQueries() {
+        try {
             OrderTask task = new OrderTask();
             int month = 10;
             int year = 1997;
             int day = 10;
             printQuery(task.ordersByDayOnMonth(month, year));
             printQuery(task.rankByDayOnMonth(month, year));
-            printQuery(task.topTenProducts(day,month,year));
+            printQuery(task.topTenProducts(day, month, year));
             printQuery(task.downTenProducts(day, month, year));
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public void printQuery(ResultSet rs) throws Exception
-    {
-        for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
-        {
+    public void printQuery(ResultSet rs) throws Exception {
+        for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
             System.out.print(rs.getMetaData().getColumnName(i) + " ");
         }
 
         System.out.println("\n-----------");
 
-        while (rs.next())
-        {
-            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
-            {
+        while (rs.next()) {
+            for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
                 System.out.print(rs.getString(i) + " ");
             }
 
--- src/main/java/usp/each/task/OrderTask.java
+++ src/main/java/usp/each/task/OrderTask.java
@@ -1,33 +1,31 @@
 package usp.each.task;
 
+import static usp.each.task.ConnectionFactory.getConnection;
+
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 
-public class OrderTask
-{
+public class OrderTask {
     /**
      * Query de vendas acumuladas de cada dia em um mes
+     *
      * @param month
      * @param year
      * @return
      */
-    public ResultSet ordersByDayOnMonth(int month, int year)
-    {
-        try
-        {
+    public ResultSet ordersByDayOnMonth(int month, int year) {
+        try {
             String sql = "SELECT o.OrderDate, COUNT(o.OrderID) as orders FROM Orders o"
                     + " WHERE date_part('month', o.OrderDate) = ? AND date_part('year', o.OrderDate) = ?"
                     + " GROUP BY o.OrderDate ORDER BY o.OrderDate";
 
-            PreparedStatement stmt = ConnectionFactory.getConnection().prepareStatement(sql);
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
             stmt.setInt(1, month);
             stmt.setInt(2, year);
             return stmt.executeQuery();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
@@ -35,33 +33,29 @@ public class OrderTask
 
     /**
      * Query de rank de vendas aculadas por dia em um mes
+     *
      * @param month
      * @param year
      * @return
      */
-    public ResultSet rankByDayOnMonth(int month, int year)
-    {
-        try
-        {
+    public ResultSet rankByDayOnMonth(int month, int year) {
+        try {
             String sql = "SELECT o.OrderDate, COUNT(o.OrderID) as orders,"
                     + " RANK() OVER (ORDER BY COUNT(o.OrderID) DESC)"
                     + " FROM Orders o"
                     + " WHERE date_part('month', o.OrderDate) = ? AND date_part('year', o.OrderDate) = ?"
                     + " GROUP BY o.OrderDate";
-            PreparedStatement stmt = ConnectionFactory.getConnection().prepareStatement(sql);
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
             stmt.setInt(1, month);
             stmt.setInt(2, year);
             return stmt.executeQuery();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-    
-    private String topTenQuery()
-    {
+
+    private String topTenQuery() {
         String sql = "SELECT p.ProductName, COUNT(o.OrderID) as orders,"
                 + " SUM(od.UnitPrice) as total, AVG(od.UnitPrice) as avg_price,"
                 + " RANK() OVER (ORDER BY COUNT(o.OrderID) DESC, SUM(od.UnitPrice) DESC,"
@@ -73,62 +67,222 @@ public class OrderTask
                 + " LIMIT 10";
         return sql;
     }
-    
+
     /**
      * Top 10 diario por produtos
+     *
      * @param day
      * @param month
      * @param year
      * @return
      */
-    public ResultSet topTenProducts(int day, int month, int year)
-    {
-        try
-        {
+    public ResultSet topTenProducts(int day, int month, int year) {
+        try {
             String sql = topTenQuery();
-            PreparedStatement stmt = ConnectionFactory.getConnection().prepareStatement(sql);
-            
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+
             SimpleDateFormat fomatter = new SimpleDateFormat("yyyy-mm-dd");
-            Date d = new Date(fomatter.parse(year+"-"+month+"-"+day).getTime());
-            
+            Date d = new Date(fomatter.parse(year + "-" + month + "-" + day).getTime());
+
             stmt.setDate(1, d);
-            
+
             return stmt.executeQuery();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-    
+
     /**
      * Down 10 diario por produtos
+     *
      * @param day
      * @param month
      * @param year
      * @return
      */
-    public ResultSet downTenProducts(int day, int month, int year)
-    {
-        try
-        {
+    public ResultSet downTenProducts(int day, int month, int year) {
+        try {
             String sql = topTenQuery().replaceAll("DESC", "ASC");
-            PreparedStatement stmt = ConnectionFactory.getConnection().prepareStatement(sql);
-            
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+
             SimpleDateFormat fomatter = new SimpleDateFormat("yyyy-mm-dd");
-            Date d = new Date(fomatter.parse(year+"-"+month+"-"+day).getTime());
-            
+            Date d = new Date(fomatter.parse(year + "-" + month + "-" + day).getTime());
+
             stmt.setDate(1, d);
-            
+
             return stmt.executeQuery();
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
-    
-    
+
+    //NEW QUERIES
+
+    /**
+     * Query de vendas acumuladas de cada dia em um mes considerando que as vendas são o valor monetário vendido
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet ordersByDayOnMonthConsideringSellsAsMoney(int month, int year) {
+        try {
+
+            String sql = "SELECT O.OrderDate, SUM(OD.quantity * OD.UnitPrice)"
+                    + " FROM Orders O, Order_Details OD"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " GROUP BY O.OrderDate"
+                    + " ORDER BY O.OrderDate";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Query de vendas acumuladas de cada dia em um mes considerando que as vendas são a quantidade de produtos vendida
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet ordersByDayOnMonthConsideringSellsAsProductsSoldQuantity(int month, int year) {
+        try {
+
+            String sql = "SELECT O.OrderDate, SUM(OD.quantity)"
+                    + " FROM Orders O, Order_Details OD"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " GROUP BY O.OrderDate"
+                    + " ORDER BY O.OrderDate";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Query de ranking de vendas acumuladas de cada dia em um mes considerando que as vendas são a quantidade de produtos vendida
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet rankOrdersByDayOnMonthConsideringSellsAsProductsSoldQuantity(int month, int year) {
+        try {
+
+            String sql = "SELECT O.OrderDate, RANK() OVER (ORDER BY SUM(OD.quantity)) rank"
+                    + " FROM Orders O, Order_Details OD"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " GROUP BY O.OrderDate"
+                    + " ORDER BY rank";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Query de ranking de vendas acumuladas de cada dia em um mes considerando que as vendas são o valor monetário vendido
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet rankOrdersByDayOnMonthConsideringSellsAsMoney(int month, int year) {
+        try {
+
+            String sql = "SELECT O.OrderDate, RANK() OVER(ORDER BY SUM(OD.quantity * OD.UnitPrice)) rank"
+                    + " FROM Orders O, Order_Details OD"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " GROUP BY O.OrderDate"
+                    + " ORDER BY rank";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Query de ranking de vendas produtos de cada dia em um mes
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet topRankProductsByDayOnMonth(int month, int year) {
+        try {
+
+            String sql = "SELECT P.ProductName, RANK() OVER(ORDER BY SUM(OD.quantity) DESC) rank,"
+                    + " SUM(OD.quantity), AVG(OD.UnitPrice), SUM(OD.quantity * OD.UnitPrice)"
+                    + " FROM Orders O, Order_Details OD, Products P"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " AND P.ProductID = OD.ProductID"
+                    + " GROUP BY P.ProductName"
+                    + " ORDER BY rank";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Query de ranking decrescente de vendas produtos de cada dia em um mes
+     *
+     * @param month
+     * @param year
+     * @return
+     */
+    public ResultSet downRankProductsByDayOnMonth(int month, int year) {
+        try {
+
+            String sql = "SELECT P.ProductName, RANK() OVER(ORDER BY SUM(OD.quantity) ASC) rank,"
+                    + " SUM(OD.quantity), AVG(OD.UnitPrice), SUM(OD.quantity * OD.UnitPrice)"
+                    + " FROM Orders O, Order_Details OD, Products P"
+                    + " WHERE date_part('month', O.OrderDate) = ? AND date_part('year', O.OrderDate) = ?"
+                    + " AND O.OrderId = OD.OrderId"
+                    + " AND P.ProductID = OD.ProductID"
+                    + " GROUP BY P.ProductName"
+                    + " ORDER BY rank";
+
+            PreparedStatement stmt = getConnection().prepareStatement(sql);
+            stmt.setInt(1, month);
+            stmt.setInt(2, year);
+            return stmt.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
